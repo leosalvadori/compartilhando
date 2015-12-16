bebdf8fb6f2bad9703f68408ac475008b50612c4
salvadori <leonardosalvadori7@gmail.com>
2015-12-16 00:04:21 -0200

    day one

--- authuser/models.py
+++ authuser/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import hashlib
 
 # Create your models here.
@@ -16,3 +17,20 @@ class usuario(models.Model):
     def save(self, *args, **kwargs):
         self.guid = hashlib.md5(str(random()).encode('utf-8')).hexdigest()
         super(usuario, self).save(*args, **kwargs)
+
+class anuncio(models.Model):
+    #usuario = models.OneToOneField(User)
+    usuario = models.ForeignKey('auth.User')
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(max_length=500)
+
+    class Meta:
+        verbose_name = 'Anúncio'
+        verbose_name_plural = 'Anúncios'
+
+    def save(self, *args, **kwargs):
+        self.usuario = User.get_username(self)
+        self.save()
+
+
+
